e4d47c19e0dbc0b00fbb88334cf2fff006424c0c
yasir <yasiralabadi@hotmail.com>
2021-02-23 16:53:37 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import Map from './Components/Map/index';
+import axios from 'axios';
+import Loader from './Components/Loader/index'
+const  App = () => {
 
-function App() {
-  return (
+    const [eventData, setEventData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(
+
+        () => {
+            setLoading(true);
+            axios.get(url)
+                .then(res => {
+                        setEventData(res.data.events)
+                        setLoading(false)
+                    })
+        },[]
+
+    )
+
+    const url = `https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?`
+
+
+
+
+
+    // const geometriesTitle = eventData.map(x => x.title)
+    // const geometriesID = eventData.map(x => x.categories[0].id)
+    // const geometriesData =  eventData && eventData.map(x => x.geometries.map( geo => geo.coordinates))
+    console.log('DATA from STATE:', eventData)
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className={'header'}>
+            <h1 > Welcome to the Wildfire Tracker...</h1>
+        </header>
+
+
+        {!loading ? <Map eventData={eventData} /> : <Loader />}
+
+
+
+
+
+
     </div>
   );
 }
